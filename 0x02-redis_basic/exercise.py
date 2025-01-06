548662c9263e48ff7bcbb7b1350d8be4c1026df0
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-01-05 20:36:16 -0800

    call history decorator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -53,3 +53,12 @@ class Cache:
     def get_int(self, key: str) -> Optional[int]:
         """Retrieve data as an integer."""
         return self.get(key, int)
+
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''Stores a value in a Redis data storage and returns the key.
+        '''
+        data_key = str(uuid.uuid4())
+        self._redis.set(data_key, data)
+        return data_key
